0b8804c3933884c4d24345423006a694c45a22f9
Jesega <jesega2000@gmail.com>
2020-06-05 21:18:37 +0200

    Puestos no except de articulos

--- P4/articulo.cpp
+++ P4/articulo.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //-----------------------------------------------------Auor-------------------------------------------------------------------------------
-Autor::Autor(const Cadena& nombre, const Cadena& apellidos, const Cadena& direccion): 
+Autor::Autor(const Cadena& nombre, const Cadena& apellidos, const Cadena& direccion) noexcept: 
 nombre_{nombre}, apellidos_{apellidos}, direccion_{direccion}{}
 
 //-----------------------------------------------------Articulo-------------------------------------------------------------------------------
@@ -17,7 +17,7 @@ autores_{as}, referencia_(ref), titulo_(tit), f_publi_(f), precio_(price)
     }
 }
 
-std::ostream& operator <<(std::ostream& os, const Articulo& art)
+std::ostream& operator <<(std::ostream& os, const Articulo& art) noexcept
 {
     //Imprimimos la referencia y el título
     os << '[' << art.referencia() <<"] "<< '\"' << art.titulo() << '\"' << ", de ";
@@ -52,7 +52,7 @@ Articulo(as, ref, tit, f, price), stock_{s}{}
 Libro::Libro(const Autores& as, const Cadena& ref, const Cadena& tit, const Fecha& f, double price,int n_pag, int s): 
 ArticuloAlmacenable(as, ref, tit, f, price, s), n_pag_{n_pag}{}
 
-void Libro::impresion_especifica(ostream& os) const
+void Libro::impresion_especifica(ostream& os) const noexcept
 {
     os << n_pag_ << " págs., "<< stock_<<  " unidades.";
 }
@@ -61,7 +61,7 @@ void Libro::impresion_especifica(ostream& os) const
 Cederron::Cederron(const Autores& as, const Cadena& ref, const Cadena& tit, const Fecha& f, double price,int tam, int s): 
 ArticuloAlmacenable(as, ref, tit, f, price, s), tam_{tam}{}
 
-void Cederron::impresion_especifica(ostream& os) const
+void Cederron::impresion_especifica(ostream& os) const noexcept
 {
     os <<  tam_ << " MB, "<< stock_<< " unidades.";
 }
@@ -70,7 +70,7 @@ void Cederron::impresion_especifica(ostream& os) const
 LibroDigital::LibroDigital(const Autores& as, const Cadena& ref, const Cadena& tit, const Fecha& f, double price, const Fecha& f_expir): 
 Articulo(as, ref, tit, f, price), f_expir_{f_expir}{}
 
-void LibroDigital::impresion_especifica(ostream& os) const
+void LibroDigital::impresion_especifica(ostream& os) const noexcept
 {
     os <<  "A la venta hasta el "<< f_expir_ <<'.';
 }
